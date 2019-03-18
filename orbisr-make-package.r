de60ab276aad3aa3355ceaa0a07e5e7774d079e4
Stanislav Vlasov <stanislav.a.vlasov@gmail.com>
2019-03-18 20:39:38 +0100

    Add harmonization. Improve line counts function.

--- orbisr-make-package.r
+++ orbisr-make-package.r
@@ -57,6 +57,9 @@ document()
 
 ## Testing
 ## --------------------------------------------------------------------------------
+detach('orbisr')
+remove.packages('orbisr')
+
 install(".")
 
 ## install_github("stasvlasov/harmonizer")
